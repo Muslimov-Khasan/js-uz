072f1f32a30699fe98d4965bcd3965a07dcb7a09
Muslimov-Xasan <muslimovxasan050@gmail.com>
2022-04-07 18:05:54 +0500

    git commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert("Asslomu Alaykum");
+
+var questinoName = prompt ("Ism Familya nima")
+
+var questinoYoung = prompt ("Nechi yosh siz")
+
+var questinoRule = prompt ("Qayer Ishlaysiz")
+
+alert("Rahmat");
+
+console.log("Nechi yosh siz,  questinoName \"Xasan Muslimov\"");
+
+console.log("Nechi yosh siz questinoYoung \"17-yosh\"")
+
+console.log("Qayer Ishlaysiz questinoRule \"O'qiman\"")
